9e03eddfdc46ddaca5d228e86168a92db19c620a
Jeremy775 <jlaisne77@gmail.com>
2022-04-21 11:26:20 +0200

    creation entités

---
+++ src/Entity/Formations.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FormationsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FormationsRepository::class)]
+class Formations
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $formation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFormation(): ?string
+    {
+        return $this->formation;
+    }
+
+    public function setFormation(string $formation): self
+    {
+        $this->formation = $formation;
+
+        return $this;
+    }
+}
