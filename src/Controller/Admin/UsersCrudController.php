ce9c3331ba37ccf97ee5f6cbce4ebcb3afe66acc
Jeremy775 <jlaisne77@gmail.com>
2022-04-29 10:52:11 +0200

    install  hasher + password type field usercrud

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -3,9 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Users;
+
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+use Symfony\Component\Security\Core\User\UserInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UsersCrudController extends AbstractCrudController
 {
@@ -21,8 +26,22 @@ class UsersCrudController extends AbstractCrudController
             TextField::new('nom'),
             TextField::new('prenom'),
             TextField::new('mail'),
-            TextField::new('mdp'),
+            TextField::new('mdp')->setFormType(PasswordType::class),
             AssociationField::new('role')->renderAsNativeWidget(),
         ];
+    }
+
+
+    /**
+     * @param Users $user
+     */
+    public function setPassword(Users $user, UserPasswordHasherInterface $passwordHasher): void
+    {
+        $pass = $user->getMdp();
+
+        $user->setMdp(
+            $hashedPassword = $passwordHasher->hashPassword($user, $pass)
+        );
+        $user->setMdp($hashedPassword);
     }
 }
