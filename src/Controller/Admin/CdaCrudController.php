e70c39bb1c24b8640b245d5361fc4ee07d667438
Jeremy775 <jlaisne77@gmail.com>
2022-05-13 16:31:31 +0200

    + configuration des champs cda

--- src/Controller/Admin/CdaCrudController.php
+++ src/Controller/Admin/CdaCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Cda;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ColorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 
 class CdaCrudController extends AbstractCrudController
 {
@@ -12,16 +17,19 @@ class CdaCrudController extends AbstractCrudController
         return Cda::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
             TextEditorField::new('description'),
+            BooleanField::new('all_day'),
+            DateField::new('start'),
+            DateField::new('end'),
+            ColorField::new('background_color')
         ];
     }
-    */
+
 
     // Faire paginiation par mois
 }
