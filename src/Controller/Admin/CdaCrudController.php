255cf2af42500aecd3b88e7d5cb7338556fee52c
Jeremy775 <jlaisne77@gmail.com>
2022-05-13 16:47:25 +0200

    + CdaCrud choicetype field

--- src/Controller/Admin/CdaCrudController.php
+++ src/Controller/Admin/CdaCrudController.php
@@ -3,15 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Cda;
+use App\Entity\Cours;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ColorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 
 class CdaCrudController extends AbstractCrudController
 {
+
     public static function getEntityFqcn(): string
     {
         return Cda::class;
@@ -20,8 +23,10 @@ class CdaCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $choices = ['Java POO', 'PHP', 'Anglais'];
+
         return [
-            TextField::new('title'),
+            ChoiceField::new('title')->setChoices(array_combine($choices, $choices)),
             TextEditorField::new('description'),
             BooleanField::new('all_day'),
             DateField::new('start'),
