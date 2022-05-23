236423a2e05eb93e3ccbd09209b4da278fdba76e
Jeremy775 <jlaisne77@gmail.com>
2022-05-23 10:10:48 +0200

    - upload fichier via easyadmin

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -5,15 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UsersCrudController extends AbstractCrudController
 {
@@ -25,7 +20,7 @@ class UsersCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        $roles = ['ROLE_ADMIN', 'ROLE_FORMATEUR', 'ROLE_USER'];
+        $roles = ['ROLE_ADMIN', 'ROLE_USER', 'ROLE_FORMATEUR'];
 
         return [
             TextField::new('nom'),
@@ -36,7 +31,36 @@ class UsersCrudController extends AbstractCrudController
                 ->allowMultipleChoices()
                 ->renderExpanded()
                 ->renderAsBadges(),
-            AssociationField::new('Formations')
+            AssociationField::new('Formations'),
+            // ImageField::new('assurance_maladie')->setUploadDir("public\uploads\assurances")
+            //     ->setFormType(FileUploadType::class)
+            //     ->setUploadedFileNamePattern('[year]/[month]/[day]/[slug]-[contenthash].[extension]')
+            //     ->setColumns(6)
+            //     ->hideOnIndex()
+            //     ->setFormTypeOptions(['attr' => [
+            //         'accept' => 'application/pdf','image/jpeg',
+            //         'image/png',
+            //         'image/webp',
+            //     ]
+            // ]),
+            // ImageField::new('carte_identite')->setUploadDir("public\uploads\carte_identite")
+            //     ->setFormType(FileUploadType::class)
+            //     ->setUploadedFileNamePattern('[year]/[month]/[day]/[slug]-[contenthash].[extension]')
+            //     ->setColumns(6)
+            //     ->hideOnIndex()
+            //     ->setFormTypeOptions(['attr' => [
+            //         'accept' => 'application/pdf','image/jpeg',
+            //         'image/png',
+            //         'image/webp',
+            //     ]
+            // ]),
+            TextField::new('assurance_maladie')->onlyOnIndex(),
+            TextField::new('carte_identite')->onlyOnIndex(),
+
+
+
+
+
         ];
     }
 }
